5751c2699934273919623232664c7a017a51c627
haddanreda <haddan.reda.net@gmail.com>
2023-05-16 15:41:12 +0100

    tweak undo mysnackbar

--- Components/MySnackBar.js
+++ Components/MySnackBar.js
@@ -4,53 +4,38 @@ import { Button, Snackbar } from 'react-native-paper';
 import { FontAwesome } from '@expo/vector-icons';
 
 const MySnackBar = ({ visible, onDismiss, snackBarMessage, undoDeleteTodo }) => (
-  <Snackbar style={styles.container} visible={visible} onDismiss={onDismiss} duration={3000}>
-    <View style={styles.innerContainer}>
-      <FontAwesome 
-        name='info-circle' 
-        color='white' 
-        style={styles.infoIcon} 
-      />
-      <Text style={styles.text}>{snackBarMessage}</Text>
-      <FontAwesome
-        name='undo'
-        color='white'
-        style={styles.undoButton}
-        onPress={() => {
-          undoDeleteTodo();
-        }}
-      />
+  <Snackbar
+    visible={visible}
+    onDismiss={onDismiss}
+    duration={3000}
+    style={styles.snackbar}
+    action={{
+      label: 'Undo',
+      onPress: undoDeleteTodo,
+    }}
+  >
+    <View style={styles.content}>
+      <FontAwesome name="info-circle" color="white" size={20} style={styles.icon} />
+      <Text style={styles.message}>{snackBarMessage}</Text>
     </View>
   </Snackbar>
 );
 
 const styles = StyleSheet.create({
-  container: {
-    position: 'absolute',
-    bottom: 25,
-    left: 20,
-    right: 20,
+  snackbar: {
     backgroundColor: 'gray',
-    borderRadius: 15,
-    height: 30, // Adjust the height value as needed
-    paddingHorizontal: 16, // Adjust the padding value as needed
-    justifyContent: 'center', // Center items vertically
-    alignItems: 'center', // Center items horizontally
+    borderRadius: 5,
   },
-  innerContainer: {
+  content: {
     flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'space-between',
   },
-  text: {
+  icon: {
+    marginRight: 8,
+  },
+  message: {
     color: 'white',
-    fontSize: 15,
-  },
-  undoButton: {
-    fontSize:15
-  },
-  infoIcon: {
-    fontSize:17
+    fontSize: 14,
   },
 });
 
